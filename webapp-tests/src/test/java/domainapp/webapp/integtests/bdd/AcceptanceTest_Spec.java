695c310d056f54a679a81fb01edfc51b55f4e7f3
Dan Haywood <dan@haywood-associates.co.uk>
2024-07-08 23:05:24 +0100

    adds ScenarioContext

--- webapp-tests/src/test/java/domainapp/webapp/integtests/bdd/AcceptanceTest_Spec.java
+++ webapp-tests/src/test/java/domainapp/webapp/integtests/bdd/AcceptanceTest_Spec.java
@@ -1,26 +1,18 @@
 package domainapp.webapp.integtests.bdd;
-import org.apache.causeway.core.config.presets.CausewayPresets;
-import org.apache.causeway.core.runtimeservices.CausewayModuleCoreRuntimeServices;
-import org.apache.causeway.persistence.jdo.datanucleus.CausewayModulePersistenceJdoDatanucleus;
-import org.apache.causeway.security.bypass.CausewayModuleSecurityBypass;
-import org.apache.causeway.testing.fixtures.applib.CausewayModuleTestingFixturesApplib;
-import org.apache.causeway.testing.specsupport.applib.CausewayModuleTestingSpecSupportApplib;
+
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
+
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.transaction.annotation.Transactional;
 
-import domainapp.modules.simple.SimpleModule;
 import domainapp.webapp.integtests.WebAppIntegTestAbstract;
+import domainapp.webapp.integtests.bdd.glue.ScenarioContext;
 import io.cucumber.junit.platform.engine.Constants;
+
 @SpringBootConfiguration
 @EnableAutoConfiguration
 @Suite
@@ -31,6 +23,8 @@ import io.cucumber.junit.platform.engine.Constants;
 @ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME,value = "@main")
 @ConfigurationParameter(key = Constants.EXECUTION_DRY_RUN_PROPERTY_NAME,value = "false")
 @ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME,value = "pretty, html:target/cucumber-report/cucumber.html")
+@Import({
+})
 //@Import({
 //
 //    CausewayModuleCoreRuntimeServices.class,
--- webapp-tests/src/test/java/domainapp/webapp/integtests/bdd/glue/SimpleObjectsSteps.java
+++ webapp-tests/src/test/java/domainapp/webapp/integtests/bdd/glue/SimpleObjectsSteps.java
@@ -1,11 +1,12 @@
 package domainapp.webapp.integtests.bdd.glue;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.assertj.core.api.Assertions;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 import domainapp.modules.simple.dom.so.SimpleObject;
@@ -19,47 +20,57 @@ import jakarta.inject.Inject;
 
 @CucumberContextConfiguration
 @SpringBootTest(classes = AcceptanceTest_Spec.class)
-public class SimpleObjectsSteps extends AcceptanceTest_Spec{
-	@Inject
-	SimpleObjects simpleObjects;
-	
-	Object lastReferenced = null;
-	List resultList;
-@Given("There are {int} SimpleObjects")
-public void there_are_simple_objects(Integer int1) {
-        final SimpleObject fred = wrap(simpleObjects).create("Fred");
-	for(int i = 0; i < int1; i++) {
-		wrap(simpleObjects).create("object" + int1);
+public class SimpleObjectsSteps extends AcceptanceTest_Spec {
 
-	}
-	lastReferenced = simpleObjects;
+    @Inject
+    SimpleObjects simpleObjects;
 
-}
-@When("I List them")
-public void i_list_them() throws IllegalAccessException, InvocationTargetException, NoSuchMethodException, SecurityException {
-	resultList = (List)lastReferenced.getClass().getMethod("listAll", null).invoke(lastReferenced, null);
-}
-@Then("I count {int} SimpleObjects")
-public void i_count_simple_objects(Integer int1) {
-	assertEquals(int1, resultList.size());
-}
+    Object lastReferenced = null;
+    List resultList;
 
-static class Dog {
-	public String name;
-}
+    @Inject ScenarioContext scenarioContext;
 
-Dog myDog = null;
-@Given("I have a dog")
-public void i_have_a_dog() {
-	myDog = new Dog();
+    @Given("There are {int} SimpleObjects")
+    public void there_are_simple_objects(Integer num) {
 
-}
-@When("I name my dog {string}")
-public void i_name_my_dog(String string) {
-	myDog.name = string;
-}
-@Then("my dog's name is {string}")
-public void my_dog_s_name_is(String string) {
-	assertEquals(myDog.name, string);
-}
+        for (int i = 0; i < num; i++) {
+            wrap(simpleObjects).create("object " + i);
+
+        }
+        lastReferenced = simpleObjects;
+
+    }
+
+    @When("I List them")
+    public void i_list_them() throws IllegalAccessException, InvocationTargetException, NoSuchMethodException, SecurityException {
+        resultList = (List) lastReferenced.getClass().getMethod("listAll", null).invoke(lastReferenced, null);
+    }
+
+    @Then("I count {int} SimpleObjects")
+    public void i_count_simple_objects(Integer int1) {
+        assertEquals(int1, resultList.size());
+    }
+
+    static class Dog {
+        public String name;
+    }
+
+    Dog myDog = null;
+
+
+    @Given("I have a dog")
+    public void i_have_a_dog() {
+        Assertions.assertThat(scenarioContext).isNotNull();
+        myDog = new Dog();
+    }
+
+    @When("I name my dog {string}")
+    public void i_name_my_dog(String string) {
+        myDog.name = string;
+    }
+
+    @Then("my dog's name is {string}")
+    public void my_dog_s_name_is(String string) {
+        assertEquals(myDog.name, string);
+    }
 }
